e77747565245a61bc40a2ee0deab520e23eeee8c
Rayden98 <96914779+Rayden98@users.noreply.github.com>
2023-05-23 10:42:14 -0600

    Add files via upload

---
+++ art.py
@@ -0,0 +1,6 @@
+logo = """ 
+ dP""b8 88   88 888888 .dP"Y8 .dP"Y8     88b 88 88   88 8b    d8 88""Yb 888888 88""Yb 
+dP   `" 88   88 88__   `Ybo." `Ybo."     88Yb88 88   88 88b  d88 88__dP 88__   88__dP 
+Yb  "88 Y8   8P 88""   o.`Y8b o.`Y8b     88 Y88 Y8   8P 88YbdP88 88""Yb 88""   88"Yb  
+ YboodP `YbodP' 888888 8bodP' 8bodP'     88  Y8 `YbodP' 88 YY 88 88oodP 888888 88  Yb
+""" 
---
+++ guess_the_number.py
@@ -0,0 +1,47 @@
+import art 
+import random 
+
+again_game = True
+while again_game == True:
+    print(art.logo)
+    print("Welcome to the game Guess The Number")
+    
+    what_level = input("Choise the level, put 'easy' or 'hard'\n").lower()
+    
+    generating = random.randint(0,50)
+    #chances = int
+    def guess_number (chances):
+        #Generating the number
+        global generating  
+    
+        #Attempts of guessing the number
+        chances_remaining = chances 
+        guessed = False
+        while  guessed == False:
+            attempt_number = int(input("Put the number that you thinks is\n"))
+            if attempt_number != generating:
+                chances_remaining -= 1
+                if chances_remaining == 0:
+                    print(f"The number of attemps remaining: 0")
+                    print("Sorry you lose")
+                    guessed = True
+                elif attempt_number < generating:
+                    print(f"The number of attemps remaining: {chances_remaining}") 
+                    print("The number is higher")
+                else:
+                    print(f"The number of attemps remaining: {chances_remaining}") 
+                    print("The number is lower")
+            else: 
+                print(f"You're right the number is {generating} You WON!!")
+                restart = print(input("Do you want to play again? press 'y' or 'n'\n"))
+                if restart == "n":
+                    again_game == False
+                else: 
+                    guessed = True
+    
+    if what_level == "easy":
+        print("You have 10 chances, guess the number")
+        guess_number (10)
+    elif what_level == "hard":
+        print("You have 5 chances, guess the number")
+        guess_number (5)
